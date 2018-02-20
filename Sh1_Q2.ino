6d371dc0c3c7659a9deb66343373ec13d9889d8c
emptyContent <36591134+emptyContent@users.noreply.github.com>
2018-02-20 20:01:03 +0200

    Add files via upload

---
+++ Sh1_Q2.ino
@@ -0,0 +1,48 @@
+
+
+ int a =4;
+ int b=5;
+ int c=6;
+ int d=7;
+
+
+       
+void setup() {
+
+        
+        pinMode(a,OUTPUT);
+        pinMode(b,OUTPUT);
+        pinMode(c,OUTPUT);
+        pinMode(d,OUTPUT);
+}
+
+void loop() {
+
+
+digitalWrite(d,HIGH);
+delay(1000);
+digitalWrite(d,LOW);
+
+digitalWrite(c,HIGH);
+delay(1000);
+digitalWrite(c,LOW);
+
+digitalWrite(b,HIGH);
+delay(1000);
+digitalWrite(b,LOW);
+
+digitalWrite(a,HIGH);
+delay(1000);
+digitalWrite(a,LOW);
+
+digitalWrite(b,HIGH);
+delay(1000);
+digitalWrite(b,LOW);
+
+digitalWrite(c,HIGH);
+delay(1000);
+digitalWrite(c,LOW);
+
+  
+
+}
