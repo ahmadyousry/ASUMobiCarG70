a58f00a4fe742fdc03b863230fbb0e23dca4c79c
ibrahimkamal1997 <36606888+ibrahimkamal1997@users.noreply.github.com>
2018-02-21 22:25:13 +0200

    Add files via upload

---
+++ traffic arduino.ino
@@ -0,0 +1,24 @@
+int ledred=1;
+int ledgreen=3;
+int ledyellow=2;
+void setup()
+ {
+  pinMode (ledred,OUTPUT);
+  pinMode (ledgreen,OUTPUT);
+  pinMode (ledyellow,OUTPUT);
+  }
+  void loop ()
+  {
+   digitalWrite (ledred,HIGH);
+   digitalWrite (ledgreen,LOW);
+   digitalWrite (ledyellow,LOW);
+   delay (2000);
+   digitalWrite (ledred,LOW);
+   digitalWrite (ledgreen,LOW);
+   digitalWrite (ledyellow,HIGH);
+    delay (2000);
+   digitalWrite (ledred,LOW);
+   digitalWrite (ledgreen,HIGH);
+   digitalWrite (ledyellow,LOW);
+   delay (2000);
+  }
