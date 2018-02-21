3db36a42ef524aea4828f36db875d7fe76b4b631
ibrahimkamal1997 <36606888+ibrahimkamal1997@users.noreply.github.com>
2018-02-21 21:24:34 +0200

    Add files via upload

---
+++ button arduino.ino
@@ -0,0 +1,19 @@
+const int led=2;
+const int button=10;
+int count =0;
+void setup()
+ {
+  pinMode (led,OUTPUT);
+  pinMode (button,INPUT);
+  
+  }
+  void loop ()
+  {
+    count = digitalRead (button);
+    if(count==HIGH)
+    digitalWrite (led,HIGH);
+
+     else 
+     digitalWrite (led,LOW);
+     
+  }
