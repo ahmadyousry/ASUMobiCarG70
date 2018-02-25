e609c0202f8852e8a65f847f612d599aaf2c6c21
ibrahimkamal1997 <36606888+ibrahimkamal1997@users.noreply.github.com>
2018-02-25 20:56:46 +0200

    Add files via upload

---
+++ ultra_sonic_arduino.ino
@@ -0,0 +1,25 @@
+const byte trigger =7;
+const byte echo =8;
+float distance ;
+unsigned long period;
+void setup ()
+  {
+    pinMode (trigger,OUTPUT);
+    pinMode (echo, INPUT);
+    Serial.begin(9600);
+  }
+  void loop ()
+    {
+      digitalWrite (trigger, LOW);
+      delayMicroseconds (3);    
+      digitalWrite (trigger, HIGH);
+      delayMicroseconds (10);  
+      digitalWrite (trigger, LOW);
+      period =pulseIn (echo,HIGH);
+      distance=float (period)/58.8;      // distance=[period /(10^6 *2)]/380*100 cm
+      Serial.println (distance);
+      delay (60);
+    }
+      
+        
+    
