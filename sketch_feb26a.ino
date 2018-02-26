4a1fa5455626f0aa6ba839060e0c53585970cdae
A97Z33 <36569544+A97Z33@users.noreply.github.com>
2018-02-26 22:13:09 +0200

    Add files via upload

---
+++ sketch_feb26a.ino
@@ -0,0 +1,23 @@
+const int led=13;
+const int on=3;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(on,INPUT);
+  pinMode(led,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(on)==HIGH)
+  {
+    digitalWrite(led,HIGH);
+    delay(2000);
+  }
+  else
+  {
+    digitalWrite(led,LOW);
+  }
+}
+   
+
