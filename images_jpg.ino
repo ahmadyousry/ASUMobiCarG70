5cd42ca129c4b0ebce23ba73ae2e1dd821ed4d15
A97Z33 <36569544+A97Z33@users.noreply.github.com>
2018-02-26 22:33:21 +0200

    Add files via upload

---
+++ images_jpg.ino
@@ -0,0 +1,30 @@
+const int red=13;
+const int yellow=12;
+const int green=11;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(red,OUTPUT);
+  pinMode(green,OUTPUT);
+  pinMode(yellow,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(red,HIGH);
+  delay(5000);
+  digitalWrite(red,LOW);
+  for(int i=0;i<=10;i++)
+  {
+    digitalWrite(yellow,HIGH);
+    delay(500);
+    digitalWrite(yellow,LOW);
+    delay(500);
+  }
+  digitalWrite(green,HIGH);
+  delay(5000);
+  digitalWrite(green,LOW);
+  
+}
+   
+
