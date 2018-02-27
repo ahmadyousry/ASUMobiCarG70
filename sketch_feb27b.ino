4857f5df2bbbd3401c038445377daad761d07729
A97Z33 <36569544+A97Z33@users.noreply.github.com>
2018-02-27 10:26:45 +0200

    Add files via upload

---
+++ sketch_feb27b.ino
@@ -0,0 +1,26 @@
+int motorpin=9;
+int ontime=2500;
+int offtime=1000;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motorpin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(motorpin,100);
+  delay(ontime);
+  analogWrite(motorpin,LOW);
+  delay(offtime);
+  analogWrite(motorpin,190);
+  delay(ontime);
+  analogWrite(motorpin,LOW);
+  delay(offtime);
+  analogWrite(motorpin,255);
+  delay(ontime);
+  analogWrite(motorpin,LOW);
+  delay(offtime);
+    
+      
+
+}
